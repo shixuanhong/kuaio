e293e3ff0ba29bcd31e8e128b652d23d55a37a0c
shixuanhong <310303892@qq.com>
2023-07-18 23:36:40 +0800

    feat: getPlatform

--- src/constants/index.js
+++ src/constants/index.js
@@ -7,3 +7,17 @@ export const KeyboardEventType = {
   KeyDown: 'keydown',
   KeyUp: 'keyup'
 }
+
+/**
+ * @enum {string}
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/NavigatorUAData/platform
+ */
+export const PlatformBrand = {
+  Windows: 'Windows',
+  MacOS: 'macOS',
+  IOS: 'iOS',
+  IPadOS: 'iPadOS',
+  Linux: 'Linux',
+  Android: 'Android',
+  Unknown: 'Unknown'
+}
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,4 @@
-import { ModifierKeys } from '../constants/index'
+import { ModifierKeys, PlatformBrand } from '../constants/index'
 import { defaultConfig } from '../core/config/index'
 
 export function keyEqualTo(key1, key2) {
@@ -18,5 +18,35 @@ export function getModifierKeyPressed(modifier, e) {
     return e.getModifierState(ModifierKeys.CapsLock)
   } else {
     return e.getModifierState(modifier)
+  }
+}
+
+/**
+ * Get the operating system of the current user agent
+ * @returns {string}
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/NavigatorUAData/platform
+ */
+export function getPlatform() {
+  if (navigator.userAgentData) {
+    return navigator.userAgentData.platform
+  } else {
+    // The judgment here is still not completely accurate.
+    if (navigator.userAgent.indexOf('Windows') > -1) {
+      return PlatformBrand.Windows
+    } else if (navigator.userAgent.indexOf('iPhone OS') > -1) {
+      return PlatformBrand.IOS
+    } else if (navigator.userAgent.indexOf('iPad') > -1) {
+      return PlatformBrand.IPadOS
+    } else if (navigator.userAgent.indexOf('Mac OS X') > -1) {
+      // On iOS or iPad OS, the userAgent string also contains "Mac OS X", so the judgment is placed here.
+      return PlatformBrand.MacOS
+    } else if (navigator.userAgent.indexOf('Android') > -1) {
+      return PlatformBrand.Android
+    } else if (navigator.userAgent.indexOf('Linux') > -1) {
+      // On Android, the userAgent string also contains "Linux", so the judgment is placed here.
+      return PlatformBrand.Linux
+    } else {
+      return PlatformBrand.Unknown
+    }
   }
 }
